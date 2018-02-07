275011caed92a7b206662b4f99e78bf3f306e826
Michael Carosino <mcaros@uw.edu>
2018-02-07 00:41:30 -0800

    Switched to using bitgrail api and added python3 compat

--- bitgrail_parser.py
+++ bitgrail_parser.py
@@ -1,165 +1,129 @@
-#Donation address: xrb_33jc7cshocfi3pmx7jfnux4z16dqu1b59y9jp9u6ui3naipfzymysc8hxwst
+# Donation address: xrb_33jc7cshocfi3pmx7jfnux4z16dqu1b59y9jp9u6ui3naipfzymysc8hxwst
 
-#Python script to scrape bitgrail order history and format csv's for use to import trades to bitcoin.tax and cointracking.info websites
+# Python script to scrape bitgrail API order history and format csv's for use to import trades to bitcoin.tax and cointracking.info websites
 
-#NOTE: Bitgrail does not store a year with its date string.
-#So this script assumes you joined bitgrail sometime AFTER end of March 2017 and you're running this script BEFORE end of March 2018.
+# README:
+# Tested on ubuntu but should work on Windows too - check that the output csv file look formatted okay in notepad though!
+# tested on python 2.7 but I've made some tweaks to make it compatible with python 3+
 
-#README:
-#Tested on ubuntu but should work on Windows too.
-
-#outputs: bitgrailtrades_cointrackingformat.csv and bitgrailtrades_bitcointaxformat.csv
+# outputs: bitgrailtrades_cointrackingformat.csv and bitgrailtrades_bitcointaxformat.csv
 
 # INSTALL INSTRUCTIONS:
 # python 2.7 https://www.python.org/download/releases/2.7/ (or apt-get python in ubuntu)
-# for UBUNTU install chromium, for windows chrome
-# also need to download the chromedriver https://sites.google.com/a/chromium.org/chromedriver/
-# On UBUNTU place this in /usr/local/bin/ for WINDOWS place in c:\Windows\
 
 # RUN by typing in cmd/terminal: python bitgrail_parsery.py
 
 
-
-
 import csv
 import time
-from selenium import webdriver
+import requests
+import hmac
+import hashlib
+from builtins import input
 
-timezone = raw_input("Please enter timezone in UTC (e.g. for PST enter: -8)")
-timezone = float(timezone)
-timezone = '%0.2i'%timezone + '00'
+url = 'https://api.bitgrail.com/v1/lasttrades'
 
-driver = webdriver.Chrome()
-driver.get("https://bitgrail.com")
+timezone = input("Please enter timezone in UTC (e.g. for PST enter: -8)")
+timezone_v = float(timezone)
+timezone = '%0.2i'%timezone_v + '00'
 
+print("This script requires an API key generated from the https://bitgrail.com/api-keys website")
+print("The key does not need to have trading or withdrawal enabled and you can delete it immediately after running script")
 
-raw_input('Press enter once logged in successfully')
+mykey = input('Type or C/P the API Key \n')
+mysecret = input('Type or C/P the API Secret \n')
 
-
-driver.get("https://bitgrail.com/orders")
-driver.implicitly_wait(10)
-
-#If missing some trades, maybe page isnt loading fast enough, could sleep here
-#time.sleep(10)
-
-x = driver.find_elements_by_class_name("table2")[1]
-z=x.text
-lines = z.splitlines()
-#remove beginning shit
-lines = lines[1:]
+#API doesn't provide fee amount so we must calc it manually
+#Bitgrail fee has always been 0.2%, hopefully doesnt change in future
+fee_percent = 0.002
 
 
-#Loop thru and parse the trades into a list of lists
+def nonce():
+    return int(time.time()*1000)
 
+n=nonce()
+payload = {'nonce':n}
+headers = {'KEY':mykey,'SIGNATURE':'0'}
+
+signature = hmac.new(mysecret, 'nonce=%i' % n, digestmod=hashlib.sha512)
+
+headers['SIGNATURE'] = signature.hexdigest()
+
+#Standard requests method
+
+response = requests.request('POST',url,data=payload,headers=headers,timeout=10.0)
+
+js=response.json()
 z = []
-for i in range(0,len(lines)):
-    #Check if this is a buy or sell
-    if lines[i].find('Sell') > -1:
-        #this means we are selling the second coin in the pair (e.g. BTC/XRB -> Selling XRB)
-        start_coin = lines[i][lines[i].find('-')+1:lines[i].find(' ')]
-        end_coin = lines[i][0:lines[i].find('-')]
-        tmp = lines[i][lines[i].find('Sell')+5:]
-        tmp = tmp.split()
-        price_in_second_coin_units = float(tmp[2])
-        price_in_first_coin_units = 1.0/price_in_second_coin_units
-        amount_first_coin = float(tmp[0])
-        amount_second_coin = float(tmp[6])
-        fee_cur = end_coin
+if js['success'] == 1:
+    j=js['response']
+    #convret dictionary to list and then sort it
+    j = list(j.values())
+    j = sorted(j,key = lambda d: d['date'])
+    #Loop thru and parse the trades into a list of lists
+    for i in j:
+        market = i['market']
+        amount = i['amount']
+        price = i['price']
+        datestamp = time.strftime('%Y-%m-%d %H:%M:%S',time.gmtime( float(i['date']) + timezone_v*60*60) )
+        action = i['type']
+        fee = amount*price*fee_percent
+        #On cointracking, fee should already be included in the amounts!
+        if action == 'sell':
+            start_coin = market[market.find('-')+1:]
+            end_coin = market[0:market.find('-')]
+            amount_first_coin = amount
+            amount_second_coin = amount*price - fee
+            fee_cur = end_coin
+        else:
+            start_coin = market[0:market.find('-')]
+            end_coin = market[market.find('-')+1:]
+            amount_second_coin = amount
+            amount_first_coin = amount*price + fee
+            fee_cur = start_coin
+            
+        z.append(["Trade","%f"%amount_second_coin,"%s"%end_coin,"%f"%amount_first_coin,"%s"%start_coin,"%f"%fee,"%s"%fee_cur,"Bitgrail","","","%s"%datestamp])
         
-    elif lines[i].find('Buy') > -1:
-        #this means we are Buying the second coin and selling the first (e.g. BTC/XRB -> Buying XRB)
-        end_coin = lines[i][lines[i].find('-')+1:lines[i].find(' ')]
-        start_coin = lines[i][0:lines[i].find('-')]
-        tmp = lines[i][lines[i].find('Buy')+4:]
-        tmp = tmp.split()
-        price_in_first_coin_units = float(tmp[2])
-        price_in_second_coin_units = 1.0/price_in_first_coin_units
-        amount_first_coin = float(tmp[6])
-        amount_second_coin = float(tmp[0])
-        fee_cur = start_coin
-    else:
-        print "WTF"
-        break
-    
-    
-    fee = float(tmp[4])
-    #grab date str and
-    tmp2 = lines[i].split()
-    month = float(tmp2[1][:2])
-    if month > 6:
-        date = tmp2[1]+'/2017'
-    else:
-        date = tmp2[1]+'/2018'
-    
-    time = tmp2[2]
-    
-    
-    month = float(tmp2[1][:2])
-    #assuming you joined bitgrail april 2017
-    if month > 3:
-        year = '2017'
-    else:
-        year = '2018'
-    
-    month = tmp2[1][:2]
-    day = tmp2[1][3:5]
-    
-    date = year+'-'+month+'-'+day+' '+tmp2[2]  
-    
-    #store in the list
-    z.insert(0,["Trade","%f"%amount_second_coin,"%s"%end_coin,"%f"%amount_first_coin,"%s"%start_coin,"%f"%fee,"%s"%fee_cur,"Bitgrail","","","%s"%date])
-
-
-z.insert(0,["Type","Buy","Cur.","Sell","Cur.","Fee","Cur.","Exchange","Group","Comment","Date"])
-
-with open("bitgrailtrades_cointrackingformat.csv","wb") as f:
-    writer = csv.writer(f)
-    writer.writerows(z)
-    
+        
+    z.insert(0,["Type","Buy","Cur.","Sell","Cur.","Fee","Cur.","Exchange","Group","Comment","Date"])
+    with open("bitgrailtrades_cointrackingformat.csv","w") as f:
+        writer = csv.writer(f)
+        writer.writerows(z)         
+else:
+    print("Failed to grab trades, check your keys and internet connection")
     
 z2 = []
-
-for i in range(0,len(lines)):
-    #Check if this is a buy or sell
-    tl = []
-    #grab date str and
-    tmp = lines[i].split()
-    month = float(tmp[1][:2])
-    #assuming you joined bitgrail after june 2017
-    if month > 3:
-        year = '2017'
-    else:
-        year = '2018'
-    
-    month = tmp[1][:2]
-    day = tmp[1][3:5]
-    
-    date = year+'-'+month+'-'+day+' '+tmp[2]+' '+timezone
-    
-    
-    
-    if lines[i].find('Sell') > -1:
-        action = 'SELL'
-        tmp2 = lines[i][lines[i].find('Sell')+5:]
-        tmp2 = tmp2.split()        
-    else:
-        action = 'BUY'
-        tmp2 = lines[i][lines[i].find('Buy')+4:]
-        tmp2 = tmp2.split()        
-    
-    symbol = lines[i][lines[i].find('-')+1:lines[i].find(' ')]
-    volume = float(tmp2[0])
-    currency = lines[i][0:lines[i].find('-')]
-    price = float(tmp2[2])
-    fee = float(tmp2[4])
- 
- 
-    z2.insert(0,[date,'Bitgrail',action,symbol,'%f'%volume,currency,'%f'%price,'%f'%fee])
+if js['success'] == 1:
+    j=js['response']
+    #convret dictionary to list and then sort it
+    j = list(j.values())
+    j = sorted(j,key = lambda d: d['date'])
+    #Loop thru and parse the trades into a list of lists
+    for i in j:
+        market = i['market']
+        volume = i['amount']
+        price = i['price']
+        datestamp = time.strftime('%Y-%m-%d %H:%M:%S',time.gmtime( float(i['date']) + timezone_v*60*60) )
+        datestamp = datestamp + ' ' + timezone
+        action = i['type']
+        fee = volume*price*fee_percent
+        symbol = market[market.find('-')+1:]
+        currency = market[0:market.find('-')]        
+        #On cointracking, fee should NOT be included in amounts already.
+        if action == 'buy':
+            action = 'BUY'
+        else:
+            action = 'SELL'
+        z2.append([datestamp,'Bitgrail',action,symbol,'%f'%volume,currency,'%f'%price,'%f'%fee])
 
 
-z2.insert(0,['Date','Source','Action','Symbol','Volume','Currency','Price','Fee'])
+    z2.insert(0,['Date','Source','Action','Symbol','Volume','Currency','Price','Fee'])
 
 
-with open("bitgrailtrades_bitcointaxformat.csv","wb") as f:
-    writer = csv.writer(f)
-    writer.writerows(z2)
+    with open("bitgrailtrades_bitcointaxformat.csv","w") as f:
+        writer = csv.writer(f)
+        writer.writerows(z2)
+     
+else:
+    print("Failed to grab trades, check your keys and internet connection")
+        
